74b645e87999dc884ffbeaf0612862ecb9f9195a
Takeshi Gomez <takegomez03@gmail.com>
2023-08-10 21:47:01 -0300

    Arreglado

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -6,16 +6,17 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-const nombre = document.getElementById("nombre").value;
-const apellido = document.getElementById("apellido").value;
-const email = document.getElementById("email").value;
-const pass1 = document.getElementById("password1").value;
-const pass2 = document.getElementById("password2").value;
-const check = document.getElementById("terminos");
+
 
 const btn = document.getElementById("regBtn");
 
 function enviar() {
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const pass1 = document.getElementById("password1").value;
+    const pass2 = document.getElementById("password2").value;
+    const check = document.getElementById("terminos");
     if (!nombre || !email || !apellido || !pass1 || !pass2 || !check.checked || pass1.length < 6 || pass1 !== pass2) {
         showAlertError()
         return false;
